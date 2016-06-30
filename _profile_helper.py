30047d863385df43c258d863bec0dc987108f161
dsolis450 <dsolis450@gmail.com>
2016-06-30 17:23:21 -0500

    Added more header functions to HEADER_FN

--- _profile_helper.py
+++ _profile_helper.py
@@ -3,7 +3,12 @@
 HEADER_FN = {
   'ExtPropertyID': val_propertyid,
   'AssetClass': val_assetclass,
+  'AssetName': val_assetname,
+  'AssetNumber': val_assetnumber,
+  'SerialNumber': val_serialnumber,
   'AssetRank': val_assetrank,
+  'Make': val_make,
+  'Model': val_model,
   'AssetStatus': val_assetstatus,
   'AssetKeyword': val_assetkeyword
 }
