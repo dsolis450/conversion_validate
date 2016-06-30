72bb6e54a9ea541c2371ca2ff9b231f613e7153d
dsolis450 <dsolis450@gmail.com>
2016-06-30 16:40:02 -0500

    Create val_helper.py

---
+++ val_helper.py
@@ -0,0 +1,12 @@
+#Helper file that holds the header-function dictionary and the apply function
+
+HEADER_FUNC_DICT = {
+  'ExtPropertyID': val_propertyid,
+  'AssetClass': val_assetclass,
+  'AssetRank': val_assetrank,
+  'AssetStatus': val_assetstatus,
+  'AssetKeyword': val_assetkeyword
+}
+
+def apply(valFn, header, ows, nws, range_expr, cur):
+  return valFn(header, ows, nws, range_expr, cur)
